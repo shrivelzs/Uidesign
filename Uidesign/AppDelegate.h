269c88c5e1e32ddfe0836dd94a85300022257ec2
Shu Zhang <Shu@Shus-MacBook-Pro.local>
2015-02-25 19:24:58 -0500

    Initial Commit

---
+++ Uidesign/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Uidesign
+//
+//  Created by Shu Zhang on 2/25/15.
+//  Copyright (c) 2015 Shu Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Uidesign/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Uidesign
+//
+//  Created by Shu Zhang on 2/25/15.
+//  Copyright (c) 2015 Shu Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
